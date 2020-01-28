1d3e0cf3b39191701d44aff40b1c91bdb3bd40d4
Mshary <m.alsharekh94@gmail.com>
2020-01-28 16:01:07 +0300

    Test assumes wrong current date

--- flights/tests.py
+++ flights/tests.py
@@ -94,8 +94,8 @@ class BookingDetails(APITestCase):
 
 		Booking.objects.create(flight=flight1, date="2018-01-01", user=user1, passengers=2)
 		Booking.objects.create(flight=flight2, date="2019-01-01", user=user1, passengers=2)
-		Booking.objects.create(flight=flight1, date="2020-01-01", user=user2, passengers=2)
-		Booking.objects.create(flight=flight2, date="2021-01-01", user=user2, passengers=2)
+		Booking.objects.create(flight=flight1, date="2031-01-01", user=user2, passengers=2)
+		Booking.objects.create(flight=flight2, date="2031-01-01", user=user2, passengers=2)
 
 	def test_url_works(self):
 		response = self.client.post(reverse('login'), {"username" : "laila", "password": "1234567890-="})
